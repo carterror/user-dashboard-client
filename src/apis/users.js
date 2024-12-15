13d6ec49df37fdd107d649c3081ff4772cb5829e
Carlos Brayan Rámila Chorens <carlos.bramila98@gmail.com>
2024-12-15 16:36:15 -0500

    estadisticas dashboard

--- src/apis/users.js
+++ src/apis/users.js
@@ -19,4 +19,23 @@ const getUsers = async () => {
   }
 }
 
-export { getUsers }
+const getStats = async () => {
+  const url = `${API_URL}/users/stats`
+  const myHeaders = new Headers()
+
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow"
+  }
+
+  try {
+    const response = await fetch(url, requestOptions);
+    const result = await response.json();
+    return result
+  } catch (error) {
+    return {error: error.text}
+  }
+}
+
+export { getUsers, getStats }
